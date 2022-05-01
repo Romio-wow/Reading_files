9a90163e74144ae215a157381bc883c0ff36d93b
Roman Kravchenko <roman.me@mail.ru>
2022-05-01 20:28:34 +0300

    -import commit

--- src/test/java/guru/qa/ParsingZipFiles.java
+++ src/test/java/guru/qa/ParsingZipFiles.java
@@ -6,13 +6,10 @@ import com.opencsv.CSVReader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
